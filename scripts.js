ba830f19732a32ec62421886be7ffbe5298358e4
Eunseo Yang <55356269+eunseo-yang@users.noreply.github.com>
2023-07-26 16:56:34 +0900

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,7 +1,17 @@
 function showDetails(projectNumber) {
     const projectDetails = document.getElementById(`project-details-${projectNumber}`);
+    const project = document.getElementById(`project-${projectNumber}`);
     if (projectDetails.style.display === 'none') {
         projectDetails.style.display = 'block';
+        if (project.classList.contains('left')) {
+            projectDetails.style.left = 0;
+            projectDetails.style.width = '80%';
+            projectDetails.style.margin-right = 'auto';
+        } else {
+            projectDetails.style.right = 0;
+            projectDetails.style.width = '80%';
+            projectDetails.style.margin-left = 'auto';
+        }
     } else {
         projectDetails.style.display = 'none';
     }
