7f955e7c00d52c68c6b539e1d473375f28df2ab2
Eunseo Yang <55356269+eunseo-yang@users.noreply.github.com>
2023-07-26 17:06:03 +0900

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,17 +1,25 @@
 function showDetails(projectNumber) {
     const projectDetails = document.getElementById(`project-details-${projectNumber}`);
     const project = document.getElementById(`project-${projectNumber}`);
-    
     if (projectDetails.style.display === 'none') {
         projectDetails.style.display = 'block';
-        project.style.width = '80%';
-        if (project.classList.contains('left')) {
-            project.style.marginRight = 'auto';
-            project.style.marginLeft = '0';
+        const boundingRect = project.getBoundingClientRect();
+        const leftSpace = boundingRect.left;
+        const rightSpace = window.innerWidth - boundingRect.right;
+        
+        if (leftSpace > rightSpace) {
+            // 왼쪽에 더 많은 공간이 있는 경우
+            projectDetails.style.left = 0;
+            projectDetails.style.right = 'auto';
+            projectDetails.style.width = 'calc(80% - 20px)';
+        } else {
+            // 오른쪽에 더 많은 공간이 있는 경우
+            projectDetails.style.right = 0;
+            projectDetails.style.left = 'auto';
+            projectDetails.style.width = 'calc(80% - 20px)';
         }
     } else {
         projectDetails.style.display = 'none';
-        project.style.width = 'initial';
     }
 }
 
