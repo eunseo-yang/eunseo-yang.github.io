8fe162a1510b9f0f0b868e2d3784a6fcaf7f3db8
Eunseo Yang <55356269+eunseo-yang@users.noreply.github.com>
2023-07-27 11:37:32 +0900

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -4,24 +4,8 @@ function showDetails(projectNumber) {
     
     if (projectDetails.style.display === 'none') {
         projectDetails.style.display = 'block';
-        const rect = project.getBoundingClientRect();
-        const projectLeft = rect.left;
-        const projectRight = window.innerWidth - rect.right;
-        
-        if (projectLeft < projectRight) {
-            project.style.width = '80%';
-            project.style.left = '0';
-            project.style.right = '';
-        } else {
-            project.style.width = '80%';
-            project.style.right = '0';
-            project.style.left = '';
-        }
     } else {
         projectDetails.style.display = 'none';
-        project.style.width = '';
-        project.style.left = '';
-        project.style.right = '';
     }
 }
 
