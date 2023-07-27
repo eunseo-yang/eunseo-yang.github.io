43276ace9293daebf35e8f8af82e723da79e8fcd
Eunseo Yang <55356269+eunseo-yang@users.noreply.github.com>
2023-07-27 17:40:14 +0900

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,10 +1,28 @@
-function showDetails(projectNumber) {
-    var projectDetails = document.getElementById("project-details-" + projectNumber);
-    if (projectDetails.style.display === "block") {
-        projectDetails.style.display = "none";
-    } else {
-        projectDetails.style.display = "block";
-    }
+const projectsWrapper = document.querySelector('.projects-wrapper');
+const prevArrow = document.getElementById('prev');
+const nextArrow = document.getElementById('next');
+const projectWidth = 48; // The width percentage of each project item
+
+let currentPosition = 0;
+const numProjectsPerPage = 4;
+const totalProjects = 8;
+
+prevArrow.addEventListener('click', () => {
+  if (currentPosition > 0) {
+    currentPosition -= projectWidth * numProjectsPerPage;
+    updateProjectsPosition();
+  }
+});
+
+nextArrow.addEventListener('click', () => {
+  if (currentPosition < (totalProjects - numProjectsPerPage) * projectWidth) {
+    currentPosition += projectWidth * numProjectsPerPage;
+    updateProjectsPosition();
+  }
+});
+
+function updateProjectsPosition() {
+  projectsWrapper.style.transform = `translateX(-${currentPosition}%)`;
 }
 
 document.addEventListener("DOMContentLoaded", function() {
