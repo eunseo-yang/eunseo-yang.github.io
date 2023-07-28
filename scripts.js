bfa68bf845a96e9accc4d27cffcd7f24cd7ef5d6
Eunseo Yang <55356269+eunseo-yang@users.noreply.github.com>
2023-07-28 13:56:51 +0900

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,31 +1,3 @@
-const projectsWrapper = document.querySelector('.projects-wrapper');
-const prevArrow = document.getElementById('prev');
-const nextArrow = document.getElementById('next');
-const projectWidth = 23; // The width percentage of each project item
-
-let currentPosition = 0;
-const numProjectsPerPage = 4;
-const totalProjects = 8;
-
-prevArrow.addEventListener('click', () => {
-  if (currentPosition > 0) {
-    currentPosition -= projectWidth * numProjectsPerPage;
-    updateProjectsPosition();
-  }
-});
-
-nextArrow.addEventListener('click', () => {
-  if (currentPosition < (totalProjects - numProjectsPerPage) * projectWidth) {
-    currentPosition += projectWidth * numProjectsPerPage;
-    updateProjectsPosition();
-  }
-});
-
-function updateProjectsPosition() {
-  projectsWrapper.style.transform = `translateX(-${currentPosition}%)`;
-};
-
-
 document.addEventListener("DOMContentLoaded", function() {
   const homeLink = document.getElementById("home-link");
 
