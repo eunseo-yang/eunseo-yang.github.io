02604b5f680fd1ddf8a7b6c6c3f7e0910b3dc549
Eunseo Yang <55356269+eunseo-yang@users.noreply.github.com>
2024-02-07 14:26:24 +0900

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -5,3 +5,10 @@ document.addEventListener("DOMContentLoaded", function() {
     window.location.href = "https://eunseo-yang.github.io";
   });
 });
+
+document.querySelectorAll('.publication').forEach(publication => {
+    publication.addEventListener('click', function() {
+        window.location.href = this.getAttribute('data-url');
+    });
+});
+
