9b7df9b406321546887d6c6b82589a7c332e9a16
Eunseo Yang <55356269+eunseo-yang@users.noreply.github.com>
2024-02-07 14:57:06 +0900

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -12,3 +12,38 @@ document.querySelectorAll('.publication').forEach(publication => {
     });
 });
 
+var keywordsColors = {
+    "Chatbot": "#69c2b0",
+    "Collaboration": "#a569c2",
+    "Grounded Theory Approach": "#c26986",
+    "Context-aware computing": "#c2a369",
+    "Personalization": "#c2b769",
+    "LLM": "#8fc269"
+};
+
+function generateRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+function assignColorsToKeywords() {
+    var keywordElements = document.querySelectorAll('.keyword');
+    keywordElements.forEach(function (kw) {
+        var text = kw.textContent.trim();
+        if (keywordsColors[text]) {
+            kw.style.backgroundColor = keywordsColors[text];
+        } else {
+            var randomColor = generateRandomColor();
+            kw.style.backgroundColor = randomColor;
+            keywordsColors[text] = randomColor;
+        }
+    });
+}
+
+window.onload = assignColorsToKeywords;
+
+
